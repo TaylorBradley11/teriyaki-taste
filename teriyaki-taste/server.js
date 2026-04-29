ff371149de20d4438e0f80178738e74971399e50
TaylorBradley11 <taylorwow11@gmail.com>
2026-04-29 15:37:49 -0600

    Update server.js

--- teriyaki-taste/server.js
+++ teriyaki-taste/server.js
@@ -1,1 +1,34 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
 
+app.use(express.json());
+app.use(express.static("."));
+
+// load menu
+app.get("/menu", (req, res) => {
+  const data = JSON.parse(fs.readFileSync("./data.json"));
+  res.json(data.menu);
+});
+
+// save order
+app.post("/order", (req, res) => {
+  const orders = fs.existsSync("orders.json")
+    ? JSON.parse(fs.readFileSync("orders.json"))
+    : [];
+
+  orders.push(req.body);
+  fs.writeFileSync("orders.json", JSON.stringify(orders));
+
+  res.json({ success: true });
+});
+
+// get orders (admin)
+app.get("/orders", (req, res) => {
+  const orders = fs.existsSync("orders.json")
+    ? JSON.parse(fs.readFileSync("orders.json"))
+    : [];
+  res.json(orders);
+});
+
+app.listen(3000, () => console.log("Running on http://localhost:3000"));
